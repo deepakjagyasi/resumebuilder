cfd67b883c7da7832df942a4c608f9f8114d22a2
deepak jagyasi <rockingdeepak@gmail.com>
2015-08-05 20:15:06 +1000

    changing the base for grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,27 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     connect: {
         server: {
-              options: {
-                port: 8000,
-                base: 'assets'
-              }
+          options: {
+            port: 8000,
+            base: '.',
+            hostname: '*',
+            keepalive:true,
+            options: {
+                index: 'assets/app/index.html',
+                maxAge: 300000
+            },
+            middleware: function(connect, options, middlewares) {
+              // inject a custom middleware into the array of default middlewares
+              middlewares.unshift(function(req, res, next) {
+                if (req.url !== '/hello/world') return next();
+
+                res.end('Hello, world from port #' + options.port + '!');
+              });
+
+              return middlewares;
             }
+          }
+        }
       }
   });
 
